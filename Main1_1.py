68760c4ce75184ca755d224cbe0cdfe0526654ce
JonahMW9000 <149651861+JonahMW9000@users.noreply.github.com>
2023-11-29 15:58:02 -0600

    Update Main1_1.py
    Tweaks of class calls, menu looping, addition of userID in cartMenu

--- Main1_1.py
+++ Main1_1.py
@@ -3,6 +3,7 @@ import Inventory
 import Cart
 import sqlite3
 
+# function for car menu, allows menu to go back to previous.
 def cartMenu():
    while True:
     print ("""
@@ -13,26 +14,26 @@ def cartMenu():
     5. Check Out
     """)
     answer4 = input("Please Select an Option: ") 
+    userID = User.User()
     if answer4=="1": 
        break
     elif answer4=="2":
       viewCa = Cart.Cart()
-      viewCa.view_cart()
+      viewCa.view_cart(userID.getUserID())
     elif answer4=="3":
       addCa = Cart.Cart()
       isbnCa = input("Enter the ISBN of the item you would like to add to your cart: ")
-      addCa.add_to_cart(isbnCa)
+      addCa.add_to_cart(userID.getUserID(), isbnCa)
     elif answer4=="4":
       removeCa = Cart.Cart()
       isbnRe = input("Enter the ISBN of the item you would like to remove from your cart: ")
-      removeCa.remove_from_cart(isbnRe)
+      removeCa.remove_from_cart(userID.getUserID(), isbnRe)
     elif answer4=="5":
       checkOut = Cart.Cart()
-      checkOut.check_out()
+      checkOut.check_out(userID.getUserID())
 
     elif answer4 !="1" or "2" or "3" or "4" or "5":
       print("\n Not Valid Choice Try again") 
-
 
 def inventoryMenu():
    while True:
@@ -56,30 +57,29 @@ def inventoryMenu():
       print("\n Not Valid Choice Try again") 
 
 
-answer1 = (input)
-print("Welcome to the M&T E-Commerce Store!")
 #before login loop menu
-ans=True
-while ans:
+while True:
     print ("""
     1. Log In
     2. Create Account
     3. Logout
     """)
-    ans=answer1("Please Select an Option: ") 
-    if ans=="1": 
+    answer1 = input("Please Select an Option: ") 
+    if answer1=="1": 
        logUin = User.User()
        print("\n")
        logUin.login()
        break
-    elif ans=="2":
+    elif answer1=="2":
       print("\n")
       creatAcc = User.User()
       creatAcc.create_account()
-    elif ans=="3":
+    elif answer1=="3":
+      print("\nLogging out...\n")
       quit()
-    elif ans !="1" or "2" or "3":
+    elif answer1 !="1" or "2" or "3":
       print("\n Not Valid Choice Try again") 
+
 
 
 #after login loop menu (main menu)
@@ -96,7 +96,7 @@ while True:
 
     elif answer2=="2":
       viewACI = User.User()
-      viewACI.view_account_information
+      viewACI.view_account_information()
 
     elif answer2=="3":
       inventoryMenu()
