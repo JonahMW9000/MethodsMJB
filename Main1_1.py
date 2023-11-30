28c900a5955813190c18062e161eb3a9bf946cf6
JonahMW9000 <149651861+JonahMW9000@users.noreply.github.com>
2023-11-29 18:14:23 -0600

    Update Main1_1.py
    Removed extra instances when calling upon classes, added menu checking, made main menu into function

--- Main1_1.py
+++ Main1_1.py
@@ -3,7 +3,30 @@ import Inventory
 import Cart
 import sqlite3
 
-# function for car menu, allows menu to go back to previous.
+#after login loop menu (main menu)########################
+def mainMenu():
+  while True:
+    print ("""
+    1. Log Out
+    2. View Account Information
+    3. Inventory Informatoin
+    4. Cart Information
+    """)
+    answer2 = input("Please Select an Option: ") 
+    if answer2=="1": 
+       print("\nLogging out...\n")
+       quit()
+    elif answer2=="2":
+      loggedIN.view_account_information()
+    elif answer2=="3":
+      inventoryMenu()
+    elif answer2=="4":
+      cartMenu()
+    elif answer2 !="1" or "2" or "3" or "4":
+      print("\n Not Valid Choice Try again")
+
+
+# function for car menu, allows menu to go back to previous. ################
 def cartMenu():
    while True:
     print ("""
@@ -14,27 +37,24 @@ def cartMenu():
     5. Check Out
     """)
     answer4 = input("Please Select an Option: ") 
-    userID = User.User()
+    cart = Cart.Cart()
     if answer4=="1": 
-       break
+       mainMenu()
     elif answer4=="2":
-      viewCa = Cart.Cart()
-      viewCa.view_cart(userID.getUserID())
+      cart.view_cart(loggedIN.getUserID())
     elif answer4=="3":
-      addCa = Cart.Cart()
       isbnCa = input("Enter the ISBN of the item you would like to add to your cart: ")
-      addCa.add_to_cart(userID.getUserID(), isbnCa)
+      cart.add_to_cart(loggedIN.getUserID(), isbnCa)
     elif answer4=="4":
-      removeCa = Cart.Cart()
       isbnRe = input("Enter the ISBN of the item you would like to remove from your cart: ")
-      removeCa.remove_from_cart(userID.getUserID(), isbnRe)
+      cart.remove_from_cart(loggedIN.getUserID(), isbnRe)
     elif answer4=="5":
-      checkOut = Cart.Cart()
-      checkOut.check_out(userID.getUserID())
-
+      cart.check_out(loggedIN.getUserID())
     elif answer4 !="1" or "2" or "3" or "4" or "5":
       print("\n Not Valid Choice Try again") 
 
+
+# function for Inventory menu #####################
 def inventoryMenu():
    while True:
     print ("""
@@ -43,21 +63,22 @@ def inventoryMenu():
     3. Search Inventory
     """)
     answer3 = input("Please Select an Option: ") 
+    inventory = Inventory.Inventory()
     if answer3=="1": 
-       break
+       mainMenu()
     elif answer3=="2":
-      viewII = Inventory.Inventory()
-      print(viewII.view_inventory()) 
+      print(inventory.view_inventory()) 
     elif answer3=="3":
       searchItem = input("What would you like to search for: ")
-      searchI = Inventory.Inventory()
-      print(searchI.search_inventory(searchItem))
+      print(inventory.search_inventory(searchItem))
 
     elif answer3 !="1" or "2" or "3":
       print("\n Not Valid Choice Try again") 
 
 
-#before login loop menu
+print("\nWelcome to the M&T E-Commerce Store!")
+
+#before login loop menu ####################
 while True:
     print ("""
     1. Log In
@@ -66,10 +87,14 @@ while True:
     """)
     answer1 = input("Please Select an Option: ") 
     if answer1=="1": 
-       logUin = User.User()
+       loggedIN = User.User()
        print("\n")
-       logUin.login()
-       break
+       # checks for invalid username/password input
+       while True:
+        if loggedIN.login() == False:
+         break
+        else:
+         mainMenu()
     elif answer1=="2":
       print("\n")
       creatAcc = User.User()
@@ -81,28 +106,3 @@ while True:
       print("\n Not Valid Choice Try again") 
 
 
-
-#after login loop menu (main menu)
-while True:
-    print ("""
-    1. Log Out
-    2. View Account Information
-    3. Inventory Informatoin
-    4. Cart Information
-    """)
-    answer2 = input("Please Select an Option: ") 
-    if answer2=="1": 
-       quit()
-
-    elif answer2=="2":
-      viewACI = User.User()
-      viewACI.view_account_information()
-
-    elif answer2=="3":
-      inventoryMenu()
-
-    elif answer2=="4":
-      cartMenu()
-
-    elif answer2 !="1" or "2" or "3" or "4":
-      print("\n Not Valid Choice Try again")
